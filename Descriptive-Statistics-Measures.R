04ed25f3fb04710cbc6eef36756b2228e6703226
emahnii <ecansler@claflin.edu>
2026-01-27 09:28:08 -0600

    Add files via upload

---
+++ Descriptive-Statistics-Measures.R
@@ -0,0 +1,15 @@
+#Author:Emahni Cansler;Date:01/27/2026;Purpose: Test measures of descriptive variables
+
+
+#How to apply mean function in R:
+
+variable1 <-c(1,2,3,4,5,6,7,8,9,10)
+
+
+variable2 <-(1,2,3,4,5)
+
+variable3 <-(1:1000)
+
+mean(variable1)
+
+#[1] 5.5
