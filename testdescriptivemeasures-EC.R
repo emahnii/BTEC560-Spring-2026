e63814403f284d184bbe9d5b5debdd4f6e0962ac
emahnii <ecansler@claflin.edu>
2026-02-05 09:23:12 -0600

    Add files via upload

---
+++ testdescriptivemeasures-EC.R
@@ -0,0 +1,18 @@
+#Author:Emahni Cansler;Date:02/05/2026, Purpose: Test descriptive measures
+
+
+variable1 <-c(100:200)
+variablenew <-(150:200)
+variablenew2 <-(100:150)
+
+# Calculate mean on variablenew for points 150 to 200
+
+[1] 175
+
+# Calculate median on variablenew
+
+[1] 150
+
+# Calculate mean on variablenew for points 100 to 150
+
+[1] 125
